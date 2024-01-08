aa83c4c7bd53e22d9291c5c92422a375ff900f37
tnht95 <tnht95@gmail.com>
2024-01-09 01:05:51 +0700

    build cleanup

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
+import solidPlugin from 'vite-plugin-solid';
+// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [solid()]
+  plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin()
+  ],
+  server: {
+    port: 3000
+  },
+  build: {
+    target: 'esnext'
+  }
 });
