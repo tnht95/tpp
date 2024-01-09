a4e71b30dab6480101cc6e33b69dffd5243b2b51
tnht95 <tnht95@gmail.com>
2024-01-09 17:11:05 +0700

    app cleanup

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,8 +2,9 @@
 import { render } from 'solid-js/web';
 
 import './index.css';
-import App from './App';
+import { App } from './app';
 
 const root = document.querySelector('#root');
+if (!root) throw new Error('could not find #root element id');
 
-render(() => <App />, root!);
+render(() => <App />, root);
