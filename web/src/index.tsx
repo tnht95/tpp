4ac023a6cdd5b45e4640cc0154092d32f440c951
tnht95 <tnht95@gmail.com>
2024-01-09 21:38:18 +0700

    fix tailwind

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,9 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { MountableElement, render } from 'solid-js/web';
 
 import './index.css';
 import { App } from './app';
 
 const root = document.querySelector('#root');
-if (!root) throw new Error('could not find #root element id');
 
-render(() => <App />, root);
+render(() => <App />, root as MountableElement);
