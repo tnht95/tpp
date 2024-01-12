2ca91083603973f2bc2bc8a64e3b60f792f62b6d
Claire T <claire.tran23@gmail.com>
2024-01-13 05:33:16 +0700

    update game details layout

--- web/src/pages/game-details.tsx
+++ web/src/pages/game-details.tsx
@@ -5,9 +5,9 @@ import { GameDetailsSidebar, GameDetailsTabs } from '@/parts';
 export const GameDetails = (props: ParentProps) => (
   <div class="bg-white">
     <GameDetailsTabs />
-    <div class="container pb-10 mt-8 ml-10">
+    <div class="pb-10 mt-8 ml-10">
       <div class="md:flex">
-        <div class=" mr-4 w-9/12">{props.children}</div>
+        <div class=" mr-4 w-4/6">{props.children}</div>
         <GameDetailsSidebar />
       </div>
     </div>
