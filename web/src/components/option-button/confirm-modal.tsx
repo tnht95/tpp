a241e8d3e0dc7dc73481f758e891b09ee569e798
Claire T <claire.tran23@gmail.com>
2024-01-18 12:27:28 +0700

    replace some svgs

--- web/src/components/option-button/confirm-modal.tsx
+++ web/src/components/option-button/confirm-modal.tsx
@@ -18,21 +18,7 @@ export const ConfirmModal = (props: ConfirmModalProps) => (
           class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
           onClick={() => props.onCloseHandler()}
         >
-          <svg
-            class="w-3 h-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-            />
-          </svg>
+          <i class="fa-solid fa-x text-xs" />
           <span class="sr-only">Close modal</span>
         </button>
         <div class="px-4 py-6  text-center">
--- web/src/parts/game-details-sidebar.tsx
+++ web/src/parts/game-details-sidebar.tsx
@@ -8,22 +8,12 @@ export const GameDetailsSidebar = () => (
         <span class="text-sm lg:text-base">
           A utility-first CSS framework for rapid UI development.
         </span>
-        <div class="my-4">
+        <div class="my-4 fl">
           <a
-            class="flex items-center mb-2 text-base font-semibold text-blue-600 hover:underline"
+            class="flex items-center mb-2 text-base font-semibold text-blue-600 hover:text-blue-400"
             href="#"
           >
-            <svg
-              class="w-4 h-4 mr-2 text-gray-700"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-            </svg>
+            <i class="fa-solid fa-link mr-2" />
             github.com/tailwindcss
           </a>
         </div>
