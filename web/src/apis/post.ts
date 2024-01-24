b9f5ba64d03cc36ad5465e8bf2568d9c0b27ce12
tnht95 <tnht95@gmail.com>
2024-01-24 18:25:06 +0700

    web: post pagination

--- web/src/apis/post.ts
+++ web/src/apis/post.ts
@@ -1,10 +1,13 @@
+import { LIMIT } from '@/constant';
 import { AddPost, Post, Response } from '@/models';
 
-export const fetchPostAction = (): Promise<Post[]> =>
-  fetch(`${import.meta.env.VITE_SERVER_URL}/posts`)
+export const fetchPostAction = (offset: number): Promise<Post[]> => {
+  const baseUrl = import.meta.env.VITE_SERVER_URL;
+  return fetch(`${baseUrl}/posts?orderBy=desc&offset=${offset}&limit=${LIMIT}`)
     .then(r => r.json())
     .then((r: Response<Post[]>) => r.data)
     .catch(() => {}) as Promise<Post[]>;
+};
 
 export const addPostAction = (body: AddPost): Promise<Post> =>
   fetch(`${import.meta.env.VITE_SERVER_URL}/posts`, {
--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,17 +1,70 @@
-import { createResource, For } from 'solid-js';
+import {
+  batch,
+  createEffect,
+  createResource,
+  createSignal,
+  For,
+  onCleanup,
+  onMount
+} from 'solid-js';
+import { createStore, produce } from 'solid-js/store';
 
 import { addPostAction, fetchNewestGameAction, fetchPostAction } from '@/apis';
 import { CommentForm, PostCard, VerticalGameCard } from '@/components';
+import { OFFSET } from '@/constant';
 import { useAuth } from '@/context';
+import { Post } from '@/models';
 
 export const Dashboard = () => {
-  const [newestGames] = createResource(fetchNewestGameAction);
-  const [post, { mutate }] = createResource(fetchPostAction);
   const { utils } = useAuth();
+  const [newestGames] = createResource(fetchNewestGameAction);
+  const [currentOffset, setCurrentOffset] = createSignal(0);
+  const [postResource, { refetch }] = createResource(
+    currentOffset,
+    fetchPostAction,
+    {
+      initialValue: []
+    }
+  );
+  const [post, setPost] = createStore<Post[]>(postResource());
+  const [hasReachedBottom, setHasReachedBottom] = createSignal(false);
+
+  createEffect(() => {
+    if (postResource().length > 0) {
+      batch(() => {
+        setPost(produce(oldPost => oldPost.push(...postResource())));
+        setHasReachedBottom(false);
+      });
+    }
+  });
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    if (scrollPercentage > 90 && !hasReachedBottom()) {
+      batch(() => {
+        setCurrentOffset(offset => offset + OFFSET);
+        setHasReachedBottom(true);
+      });
+    }
+  };
+
+  onMount(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
+  onCleanup(() => {
+    window.removeEventListener('scroll', handleScroll);
+  });
 
   const onSubmitHandler = (content: string) => {
     addPostAction({ content })
-      .then(newPost => mutate([newPost, ...(post() || [])]))
+      .then(() =>
+        batch(() => {
+          setPost([]);
+          if (currentOffset() === 0) refetch(0);
+          else setCurrentOffset(0);
+        })
+      )
       .catch(() => {});
   };
 
@@ -29,7 +82,7 @@ export const Dashboard = () => {
               )}
             </div>
             <div class="flex flex-col gap-10">
-              <For each={post()}>
+              <For each={post}>
                 {post => <PostCard content={post.content} />}
               </For>
             </div>
