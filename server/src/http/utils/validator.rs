6dd4b8b0b5d88a316c974ac0066098857a392830
tnht95 <tnht95@gmail.com>
2024-01-25 15:42:33 +0700

    web: query validation err

--- server/src/http/utils/validator.rs
+++ server/src/http/utils/validator.rs
@@ -1,13 +1,13 @@
 use axum::{
     async_trait,
     body::HttpBody,
-    extract::FromRequest,
+    extract::{FromRequest, Query},
     http::{Request, StatusCode},
     BoxError,
     Json,
 };
 use serde::de::DeserializeOwned;
-use validator::Validate;
+use validator::{Validate, ValidationErrors};
 
 use crate::model::responses::HttpResponseErr;
 
@@ -27,15 +27,7 @@ where
     async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
         match Json::<T>::from_request(req, state).await {
             Ok(Json(body)) => {
-                body.validate().map_err(|e| {
-                    (
-                        StatusCode::BAD_REQUEST,
-                        Json(HttpResponseErr {
-                            code: "ERR_002".into(),
-                            msg: format!("Input validation error: [{e}]").replace('\n', ", "),
-                        }),
-                    )
-                })?;
+                body.validate().map_err(response_validation_err)?;
                 Ok(Self(body))
             }
             Err(rejection) => Err((
@@ -47,4 +39,44 @@ where
             )),
         }
     }
+}
+
+pub struct QueryValidator<T>(pub T);
+
+#[async_trait]
+impl<T, S, B> FromRequest<S, B> for QueryValidator<T>
+where
+    T: DeserializeOwned + Validate,
+    B: HttpBody + Send + 'static,
+    B::Data: Send,
+    B::Error: Into<BoxError>,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, Json<HttpResponseErr>);
+
+    async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
+        match Query::<T>::from_request(req, state).await {
+            Ok(Query(query)) => {
+                query.validate().map_err(response_validation_err)?;
+                Ok(Self(query))
+            }
+            Err(rejection) => Err((
+                rejection.status(),
+                Json(HttpResponseErr {
+                    code: "ERR_002".into(),
+                    msg: rejection.body_text(),
+                }),
+            )),
+        }
+    }
+}
+
+fn response_validation_err(e: ValidationErrors) -> (StatusCode, Json<HttpResponseErr>) {
+    (
+        StatusCode::BAD_REQUEST,
+        Json(HttpResponseErr {
+            code: "ERR_003".into(),
+            msg: format!("Input validation error: [{e}]").replace('\n', ", "),
+        }),
+    )
 }
