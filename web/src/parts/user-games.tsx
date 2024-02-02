51c49594ad99ea1cdee1c8a6ba4744dedd1310f2
tnht95 <tnht95@gmail.com>
2024-02-02 22:42:38 +0700

    web: user game auth

--- web/src/parts/user-games.tsx
+++ web/src/parts/user-games.tsx
@@ -9,12 +9,16 @@ import {
 
 import { fetchGameAction } from '@/apis';
 import { Button, GameCard, GameForm } from '@/components';
+import { useAuthCtx } from '@/context';
 
 type UserGamesProps = {
   userId: number;
 };
 
 export const UserGames = (props: UserGamesProps) => {
+  const {
+    utils: { isSameUser }
+  } = useAuthCtx();
   const [userId] = createSignal({ authorId: props.userId });
   const [games] = createResource(userId, fetchGameAction);
   const [modalRef, setModalRef] = createSignal<HTMLDivElement>();
@@ -31,25 +35,20 @@ export const UserGames = (props: UserGamesProps) => {
           <i class="fa-regular fa-paper-plane text-lg text-green-400" />
           <span class="tracking-wide">Games</span>
         </div>
-        <Button
-          title="Add new game"
-          withIcon="fa-solid fa-plus"
-          customStyle="border-green-400 text-green-500 font-bold hover:bg-green-500 hover:text-white"
-          onClickHandler={() => {
-            modal()?.show();
-          }}
-        />
+        {isSameUser(props.userId) && (
+          <Button
+            title="Add new game"
+            withIcon="fa-solid fa-plus"
+            customStyle="border-green-400 text-green-500 font-bold hover:bg-green-500 hover:text-white"
+            onClickHandler={() => {
+              modal()?.show();
+            }}
+          />
+        )}
         <GameForm ref={setModalRef} onCloseHandler={() => modal()?.hide()} />
       </div>
       <div class="flex flex-wrap items-center gap-7">
-        <Show
-          when={games()}
-          fallback={
-            <div class="flex w-full justify-center p-7 text-gray-300">
-              <div>--- Nothing to show ---</div>
-            </div>
-          }
-        >
+        <Show when={!!games()?.length} fallback={nothingMoreToShow}>
           <For each={games()}>
             {game => (
               <GameCard
@@ -66,3 +65,9 @@ export const UserGames = (props: UserGamesProps) => {
     </div>
   );
 };
+
+const nothingMoreToShow = (
+  <div class="flex w-full justify-center p-7 text-gray-300">
+    <div>--- Nothing more to show ---</div>
+  </div>
+);
