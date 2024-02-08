4c5c8b5a8bc7b331d31410028be8a0bbadf7c221
tnht95 <tnht95@gmail.com>
2024-02-08 13:47:28 +0700

    web: refactor dashboard

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -5,7 +5,8 @@ import {
   createSignal,
   For,
   onCleanup,
-  onMount
+  onMount,
+  Show
 } from 'solid-js';
 import { createStore, produce } from 'solid-js/store';
 
@@ -16,7 +17,12 @@ import {
   fetchGameAction,
   fetchPostAction
 } from '@/apis';
-import { CommentForm, PostCard, VerticalGameCard } from '@/components';
+import {
+  CommentForm,
+  LoadingSpinner,
+  PostCard,
+  VerticalGameCard
+} from '@/components';
 import { OFFSET } from '@/constant';
 import { useAuthCtx, useToastCtx } from '@/context';
 import { PostDetails, ResponseErr } from '@/models';
@@ -97,44 +103,42 @@ export const Dashboard = () => {
       ) as unknown;
 
   return (
-    <div class="flex">
-      <div class="flex flex-1 flex-col">
-        <div class="flex h-full">
-          <nav class="flex h-full w-1/2 border-r border-dashed" />
-          <main class="mb-10 flex size-full flex-col bg-white px-32 ">
-            <div class="mx-auto my-10 flex w-full">
-              {utils.isAuth() && (
-                <CommentForm onSubmitHandler={onSubmitHandler}>
-                  New post
-                </CommentForm>
-              )}
-            </div>
-            <div class="flex flex-col gap-10 ">
-              <For each={posts}>
-                {post => (
-                  <PostCard
-                    post={post}
-                    onDelete={onDeleteHandler}
-                    onEdit={onEditHandler}
-                  />
-                )}
-              </For>
-            </div>
-          </main>
-          <nav class="flex h-full w-1/2 border-l border-dashed ">
-            <div class="fixed mx-auto flex w-full flex-col overflow-y-auto px-6">
-              <p class="mt-7 p-4 text-xl font-bold text-indigo-900">
-                Newest games
-              </p>
-              <div class="flex flex-col gap-5">
-                <For each={newestGames()}>
-                  {game => <VerticalGameCard game={game} />}
-                </For>
-              </div>
-            </div>
-          </nav>
+    <div class="flex h-full">
+      <nav class="flex-2" />
+      <main class="mb-10 flex-3 flex-col border-x border-dashed px-32">
+        <div class="my-10">
+          {utils.isAuth() && (
+            <CommentForm onSubmitHandler={onSubmitHandler}>
+              New post
+            </CommentForm>
+          )}
         </div>
-      </div>
+        <div class="flex flex-col gap-10 ">
+          <Show when={!postResource.loading} fallback={<LoadingSpinner />}>
+            <For each={posts}>
+              {post => (
+                <PostCard
+                  post={post}
+                  onDelete={onDeleteHandler}
+                  onEdit={onEditHandler}
+                />
+              )}
+            </For>
+          </Show>
+        </div>
+      </main>
+      <nav class="flex flex-2">
+        <div class="fixed flex-col overflow-hidden px-10">
+          <p class="mt-7 p-4 text-xl font-bold text-indigo-900">Newest games</p>
+          <div class="flex flex-col gap-5">
+            <Show when={!newestGames.loading} fallback={<LoadingSpinner />}>
+              <For each={newestGames()}>
+                {game => <VerticalGameCard game={game} />}
+              </For>
+            </Show>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
