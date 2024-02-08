1ca4a15dde5e2e739e9333fbeb1fcab863a5f1ce
tnht95 <tnht95@gmail.com>
2024-02-08 21:20:54 +0700

    server: auth maybe

--- server/src/http/utils/auth.rs
+++ server/src/http/utils/auth.rs
@@ -39,6 +39,34 @@ where
     }
 }
 
+pub struct AuthenticationMaybe<TInternalServices: IInternalServices>(
+    pub Option<User>,
+    pub Option<bool>,
+    pub PhantomData<TInternalServices>,
+);
+
+#[async_trait]
+impl<S, TInternalServices> FromRequestParts<S> for AuthenticationMaybe<TInternalServices>
+where
+    S: Send + Sync,
+    TInternalServices: IInternalServices,
+    Arc<Server<TInternalServices>>: FromRef<S>,
+{
+    type Rejection = ();
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let state = Arc::from_ref(state);
+        let access_token = match extract_access_token(&parts.headers) {
+            Ok(access_token) => access_token,
+            Err(_) => return Ok(Self(None, None, PhantomData)),
+        };
+        match jwt::decode(access_token, &state.config.auth.jwt.secret) {
+            Ok(jwt::JwtClaim { user, is_admin, .. }) =>
+                Ok(Self(Some(user), Some(is_admin), PhantomData)),
+            Err(_) => Ok(Self(None, None, PhantomData)),
+        }
+    }
+}
+
 #[async_trait]
 impl<S, TInternalServices> FromRequestParts<S> for Server<TInternalServices>
 where
