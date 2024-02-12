ca99b3906e2a9ed9feba80925ec58f95669d73b3
tnht95 <tnht95@gmail.com>
2024-02-12 20:28:13 +0700

    web: refactor models

--- web/src/models/game.ts
+++ web/src/models/game.ts
@@ -24,8 +24,6 @@ export type GameSummary = {
   avatarUrl: string | undefined;
   upVotes: number;
   downVotes: number;
-  tags: string[] | undefined;
-  createdAt: string;
 };
 
 export type GameRequest = {
