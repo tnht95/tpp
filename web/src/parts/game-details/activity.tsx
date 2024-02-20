4da12f0404a576f034dc992c9e46223b9865440b
tnht95 <tnht95@gmail.com>
2024-02-21 00:22:20 +0700

    web: game activities

--- web/src/parts/game-details/activity.tsx
+++ web/src/parts/game-details/activity.tsx
@@ -1,9 +1,52 @@
-import { Activity } from '@/components';
+import { For, Show } from 'solid-js';
 
-export const GameDetailsActivity = () => (
-  <ol class="relative border-s border-gray-200 dark:border-gray-700">
-    <Activity title="Flowbite Application UI v2.0.0" date="Jan 20 2023" />
-    <Activity title="Flowbite Application UI v1.0.0" date="Dec 03 2022" />
-    <Activity title="Flowbite Application UI v0.1.0" date="Jun 10 2019" />
-  </ol>
-);
+import { Activity, LoadingSpinner, ShowMoreButton } from '@/components';
+import {
+  ActivitiesProvider,
+  useActivitiesCtx,
+  useGameDetailsCtx
+} from '@/context';
+import { formatTime } from '@/utils';
+
+export const GameDetailsActivity = () => {
+  const { game } = useGameDetailsCtx();
+  return (
+    <ActivitiesProvider userId={game().authorId} targetId={game().id}>
+      <GameDetailsActivityInner />
+    </ActivitiesProvider>
+  );
+};
+
+const titleMap = {
+  addedGame: 'Added new game',
+  updatedGame: 'Updated game',
+  addedPost: 'should not be here'
+};
+
+const GameDetailsActivityInner = () => {
+  const { game } = useGameDetailsCtx();
+  const {
+    activities,
+    dispatch: { fetchMore },
+    utils: { showMore, loading }
+  } = useActivitiesCtx();
+  return (
+    <Show when={!loading()} fallback={<LoadingSpinner />}>
+      <ol class="relative border-s border-gray-200">
+        <For each={activities}>
+          {activity => (
+            <Activity
+              url={`/games/${game().id}/info`}
+              title={titleMap[activity.targetType]}
+              date={formatTime(activity.createdAt)}
+              info={activity.memo}
+            />
+          )}
+        </For>
+      </ol>
+      <Show when={showMore()}>
+        <ShowMoreButton vertical onClick={fetchMore} />
+      </Show>
+    </Show>
+  );
+};
