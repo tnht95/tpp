043692c9314f5329fff3a5f806204aea4f27606d
tnht95 <tnht95@gmail.com>
2024-02-20 23:32:01 +0700

    server: udpate activity schema

--- server/migrations/20240220080540_create_activity.up.sql
+++ server/migrations/20240220080540_create_activity.up.sql
@@ -1,12 +1,13 @@
 -- Add up migration script here
-create type activity_type as enum ('game', 'post');
+create type activity_type as enum ('added_game', 'updated_game', 'added_post');
 
 create table activities (
     user_id bigint not null,
     target_type activity_type not null,
     target_id uuid not null,
-    memo varchar(300) not null,
+    memo varchar(350) not null,
     created_at timestamp (6) with time zone default now() not null
 );
 
+create index on activities (user_id);
 create index on activities (target_id);
