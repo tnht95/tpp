00b8bcd702106d5d3364cb60a47123d9b1f303ac
tnht95 <tnht95@gmail.com>
2024-02-27 18:02:17 +0700

    server: cookie extractor

--- server/src/http/utils/cookie.rs
+++ server/src/http/utils/cookie.rs
@@ -1,11 +1,15 @@
 use anyhow::{anyhow, Result};
 use axum::http::HeaderMap;
 
-pub fn extract_access_token(headers: &HeaderMap) -> Result<&str> {
-    let cookie = headers
+fn extract_cookie(headers: &HeaderMap) -> Result<&str> {
+    Ok(headers
         .get("cookie")
         .ok_or(anyhow!("cookie not found"))?
-        .to_str()?;
+        .to_str()?)
+}
+
+pub fn extract_access_token(headers: &HeaderMap) -> Result<&str> {
+    let cookie = extract_cookie(headers)?;
 
     let access_token = cookie
         .split(';')
@@ -14,4 +18,16 @@ pub fn extract_access_token(headers: &HeaderMap) -> Result<&str> {
 
     // skip "access_token="
     Ok(&access_token[13..])
+}
+
+pub fn extract_ws_ticket(headers: &HeaderMap) -> Result<&str> {
+    let cookie = extract_cookie(headers)?;
+
+    let ws_ticket = cookie
+        .split(';')
+        .find(|item| item.starts_with("ws_ticket="))
+        .ok_or(anyhow!("websocket ticket not found"))?;
+
+    // skip "ws_ticket="
+    Ok(&ws_ticket[10..])
 }
