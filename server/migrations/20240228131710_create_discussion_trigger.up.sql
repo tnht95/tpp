a69fae6687dee0f51eb542ed388061cc3d267634
tnht95 <tnht95@gmail.com>
2024-02-28 22:28:59 +0700

    server: del discussion trigger

--- server/migrations/20240228131710_create_discussion_trigger.up.sql
+++ server/migrations/20240228131710_create_discussion_trigger.up.sql
@@ -1,27 +1,30 @@
 -- Add up migration script here
--- Trigger function to insert into the notis table when a new discussion is inserted
-CREATE FUNCTION insert_noti_on_discussion_insert ()
+CREATE FUNCTION insert_delete_noti_on_discussion_insert_delete ()
     RETURNS TRIGGER
     AS $$
 DECLARE
     noti_to_user_id bigint;
 BEGIN
-    SELECT
-        author_id INTO noti_to_user_id
-    FROM
-        games
-    WHERE
-        id = NEW.game_id;
-    INSERT INTO notis (to_user_id, by_user_id, by_user_name, by_object_id, target_type, target_id, parent_target_id)
-        VALUES (noti_to_user_id, NEW.user_id, NEW.user_name, NEW.id, 'game_discussion', NEW.id, NEW.game_id);
+    IF (TG_OP = 'INSERT') THEN
+        SELECT
+            author_id INTO noti_to_user_id
+        FROM
+            games
+        WHERE
+            id = NEW.game_id;
+        INSERT INTO notis (to_user_id, by_user_id, by_user_name, by_object_id, target_type, target_id, parent_target_id)
+            VALUES (noti_to_user_id, NEW.user_id, NEW.user_name, NEW.id, 'game_discussion', NEW.id, NEW.game_id);
+    ELSIF (TG_OP = 'DELETE') THEN
+        DELETE FROM notis
+        WHERE target_id = OLD.id;
+    END IF;
     RETURN NULL;
 END;
 $$
 LANGUAGE plpgsql;
 
--- Create a trigger that inserts a record into the notis table when a new discussion is inserted
-CREATE TRIGGER discussion_insert_trigger
-    AFTER INSERT ON discussions
+CREATE TRIGGER discussion_insert_delete_trigger
+    AFTER INSERT OR DELETE ON discussions
     FOR EACH ROW
-    EXECUTE FUNCTION insert_noti_on_discussion_insert ();
+    EXECUTE FUNCTION insert_delete_noti_on_discussion_insert_delete ();
 
