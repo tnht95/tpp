aaf9232233c8c22c32de9c44581ad073be2b263e
tnht95 <tnht95@gmail.com>
2024-03-01 21:19:34 +0700

    server: fix tag noti

--- server/migrations/20240225093143_create_comment_trigger.up.sql
+++ server/migrations/20240225093143_create_comment_trigger.up.sql
@@ -8,6 +8,7 @@ DECLARE
     noti_target_id uuid;
     noti_parent_target_id uuid;
     noti_to_user_id bigint;
+    is_tag_user_same_with_author boolean;
 BEGIN
     -- init target_type, target_id
     SELECT
@@ -47,31 +48,41 @@ BEGIN
         WHERE
             id = noti_target_id;
     END IF;
+    -- tag comments
+    WITH tag_users AS (
+INSERT INTO notis (to_user_id, by_user_id, by_user_name, by_object_id, target_type, target_id, parent_target_id)
+        SELECT
+            users.id,
+            NEW.user_id,
+            NEW.user_name,
+            NEW.id,
+            CASE WHEN noti_target_type = 'comment_discussion' THEN
+                'comment_tag_discussion'::noti_type
+            WHEN noti_target_type = 'comment_blog' THEN
+                'comment_tag_blog'::noti_type
+            WHEN noti_target_type = 'comment_post' THEN
+                'comment_tag_post'::noti_type
+            END,
+            noti_target_id,
+            noti_parent_target_id
+        FROM ( SELECT DISTINCT
+                unnest(regexp_matches(NEW.content, '@([[:alnum:]]+-?[[:alnum:]]*)', 'g')) AS name) AS match_users
+            INNER JOIN users ON users.name = match_users.name
+                AND users.id != NEW.user_id
+            RETURNING
+                notis.to_user_id
+)
+    SELECT
+        TRUE INTO is_tag_user_same_with_author
+    FROM
+        tag_users
+WHERE
+    tag_users.to_user_id = noti_to_user_id;
     -- prevent noti to oneself
-    IF (noti_to_user_id != NEW.user_id) THEN
+    IF (noti_to_user_id != NEW.user_id AND is_tag_user_same_with_author is null) THEN
         INSERT INTO notis (to_user_id, by_user_id, by_user_name, by_object_id, target_type, target_id, parent_target_id)
             VALUES (noti_to_user_id, NEW.user_id, NEW.user_name, NEW.id, noti_target_type::noti_type, noti_target_id, noti_parent_target_id);
     END IF;
-    -- tag comments
-    INSERT INTO notis (to_user_id, by_user_id, by_user_name, by_object_id, target_type, target_id, parent_target_id)
-    SELECT
-        users.id,
-        NEW.user_id,
-        NEW.user_name,
-        NEW.id,
-        CASE WHEN noti_target_type = 'comment_discussion' THEN
-            'comment_tag_discussion'::noti_type
-        WHEN noti_target_type = 'comment_blog' THEN
-            'comment_tag_blog'::noti_type
-        WHEN noti_target_type = 'comment_post' THEN
-            'comment_tag_post'::noti_type
-        END,
-        noti_target_id,
-        noti_parent_target_id
-    FROM ( SELECT DISTINCT
-            unnest(regexp_matches(NEW.content, '@([[:alnum:]]+-?[[:alnum:]]*)', 'g')) AS name) AS match_users
-    INNER JOIN users ON users.name = match_users.name
-        AND users.id != NEW.user_id;
     RETURN NULL;
 END;
 $$
