15dd46119b3838adfc43f913390182d33064db14
Claire T <claire.tran23@gmail.com>
2024-03-07 13:49:24 +0700

    server: test delete, get tag blog

---
+++ server/tests/delete_blog.rs
@@ -0,0 +1,129 @@
+use axum::{
+    body::Body,
+    extract::Request,
+    http::{Method, StatusCode},
+};
+use http_body_util::BodyExt;
+use serde_json::{json, Value};
+use tower::{Service, ServiceExt};
+
+use crate::common::{gen_jwt, gen_ws_ticket, get_admin, mock_blog, mock_user, setup_app};
+
+mod common;
+#[tokio::test]
+async fn when_not_admin() {
+    let mut app = setup_app(true).await;
+    let user = mock_user(1, "me", true).await;
+    let ws_ticket = gen_ws_ticket(&user, false).await;
+    let access_token = gen_jwt(user).await;
+
+    let request = Request::builder()
+        .uri("/api/v1/blogs/3637c923-102d-4774-b933-d0b1c966a68f")
+        .method(Method::DELETE)
+        .header(
+            "cookie",
+            format!("access_token={access_token};ws_ticket={ws_ticket}"),
+        )
+        .body(Body::empty())
+        .unwrap();
+    let response = ServiceExt::<Request<Body>>::ready(&mut app)
+        .await
+        .unwrap()
+        .call(request)
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::FORBIDDEN);
+}
+
+#[tokio::test]
+async fn with_invalid_id() {
+    let mut app = setup_app(true).await;
+    let user = get_admin().await;
+    let ws_ticket = gen_ws_ticket(&user, false).await;
+    let access_token = gen_jwt(user).await;
+
+    let request = Request::builder()
+        .uri("/api/v1/blogs/3637c923-102d-4774-b933-d0b1c966a68")
+        .method(Method::DELETE)
+        .header(
+            "cookie",
+            format!("access_token={access_token};ws_ticket={ws_ticket}"),
+        )
+        .body(Body::empty())
+        .unwrap();
+    let response = ServiceExt::<Request<Body>>::ready(&mut app)
+        .await
+        .unwrap()
+        .call(request)
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    assert_eq!(
+        body,
+        json!({
+            "code": "ERR_100",
+            "msg": "Invalid Uuid"
+        })
+    )
+}
+
+#[tokio::test]
+async fn with_nonexistent_id() {
+    let mut app = setup_app(true).await;
+    let user = get_admin().await;
+    let ws_ticket = gen_ws_ticket(&user, false).await;
+    let access_token = gen_jwt(user).await;
+
+    let request = Request::builder()
+        .uri("/api/v1/blogs/3637c923-102d-4774-b933-d0b1c966a68f")
+        .method(Method::DELETE)
+        .header(
+            "cookie",
+            format!("access_token={access_token};ws_ticket={ws_ticket}"),
+        )
+        .body(Body::empty())
+        .unwrap();
+    let response = ServiceExt::<Request<Body>>::ready(&mut app)
+        .await
+        .unwrap()
+        .call(request)
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    let data = body.get("data").unwrap();
+    assert!(data.is_null());
+}
+
+#[tokio::test]
+async fn successfully() {
+    let mut app = setup_app(true).await;
+    let user = get_admin().await;
+    let ws_ticket = gen_ws_ticket(&user, false).await;
+    let access_token = gen_jwt(user).await;
+    let blog = mock_blog().await;
+
+    let request = Request::builder()
+        .uri(format!("/api/v1/blogs/{}", blog.id))
+        .method(Method::DELETE)
+        .header(
+            "cookie",
+            format!("access_token={access_token};ws_ticket={ws_ticket}"),
+        )
+        .body(Body::empty())
+        .unwrap();
+    let response = ServiceExt::<Request<Body>>::ready(&mut app)
+        .await
+        .unwrap()
+        .call(request)
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    let data = body.get("data").unwrap();
+    assert!(data.is_null());
+}
